e6926cb1360c3c153baa18f92c176872e6f92871
Sebastian Ramirez <sramirez10r@gmail.com>
2025-07-22 21:31:48 -0500

    Adding install all

---
+++ install-all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./mac-settings.sh
+./brew-install.sh
+./other-install.sh
+./setup-dotfiles.sh
--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,6 +1,7 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 echo "🧑‍💻 Stowing dotfiles..."
 stow -v --target=$HOME zsh
 mkdir -p $HOME/.config
 stow -v --target=$HOME/.config config
+$HOME/.tmux/plugins/tpm/bin/install_plugins
 echo "🚀 Stow complete!"
